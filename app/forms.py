47ff66b0765c8d49ebc3f567cc8e556dc06a61b7
Bash Khanal <bashk@me.com>
2019-12-03 21:07:26 -0500

    Sign-up feature implemented, although errors show up while logging in

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,28 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 # For each field, an object is created as a class variable in the LoginForm(forms.py) class,
 # First argument is a label/description
 # Second argument is validators(make sure textfield is not empty, etc)
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField("User Name", validators=[DataRequired()])
     password = PasswordField("Password", validators= [DataRequired()])
     rememberMe = BooleanField("Remember Me?")
     submit = SubmitField("Sign In")
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username not available')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/models.py
+++ app/models.py
@@ -1,9 +1,12 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 #User class inherits db.Model class: a base class for all models from Flask-SQLAlchemy
 #User class defines several fields as class variables
-class User(db.Model):
+class User(UserMixin, db.Model):
     id             =db.Column(db.Integer, primary_key = True)
     username       =db.Column(db.String(64), index = True, unique = True)
     email          =db.Column(db.String(120), index = True, unique = True)
@@ -15,8 +18,18 @@ class User(db.Model):
     #  This will add a post.author expression that will return the user given a post
     # The 'lazy' argument defines how the database query for the relationship will be issued
 
-def __repr__(self): #Tells python how to print objects of this(User) class
-    return '<user {}>'.format(self.username)
+    def __repr__(self): #Tells python how to print objects of this(User) class
+        return '<user {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     #####datetime.utcnow is passed as a function itself, note that it doesn't have '()'#######
