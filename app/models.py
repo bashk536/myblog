85c2e3fdaeb8b42bcb313807a7807df185d22475
Bash Khanal <bashk@me.com>
2019-12-16 01:55:41 -0500

    ready for heroku

--- app/models.py
+++ app/models.py
@@ -19,13 +19,20 @@ class User(UserMixin, db.Model):
     email          =db.Column(db.String(120), index = True, unique = True)
     password_hash  =db.Column(db.String(128))
     posts          =db.relationship('Post', backref = 'author', lazy = 'dynamic')
-    about_me       = db.Column(db.String(140))
-    last_seen      = db.Column(db.DateTime, default=datetime.utcnow)
-    followed = db.relationship(
-        'User', secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    about_me       =db.Column(db.String(140))
+    last_seen      =db.Column(db.DateTime, default=datetime.utcnow)
+    followed       =db.relationship(
+                    'User', secondary=followers,
+                    primaryjoin=(followers.c.follower_id == id),
+                    secondaryjoin=(followers.c.followed_id == id),
+                    backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+                    
+    liked          =db.relationship(
+                    'PostLike',
+                    foreign_keys='PostLike.user_id',
+                    backref='user', lazy='dynamic')
+
+    
     # relationship() is a high-level view of the relationship between users and posts
     #The backref argument defines the name of a field that will be added to the objects of the "many" 
     # class that points back at the "one" object.
@@ -43,7 +50,8 @@ class User(UserMixin, db.Model):
     
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size) 
+
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
@@ -68,6 +76,22 @@ class User(UserMixin, db.Model):
             {'reset_password': self.id, 'exp': time() + expires_in},
             app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
+    def like_post(self, post):
+        if not self.has_liked_post(post):
+            like = PostLike(user_id=self.id, post_id=post.id)
+            db.session.add(like)
+
+    def unlike_post(self, post):
+        if self.has_liked_post(post):
+            PostLike.query.filter_by(
+                user_id=self.id,
+                post_id=post.id).delete()
+
+    def has_liked_post(self, post):
+        return PostLike.query.filter(
+            PostLike.user_id == self.id,
+            PostLike.post_id == post.id).count() > 0
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -90,8 +114,14 @@ class Post(db.Model):
     body        = db.Column(db.String(140))
     timestamp   = db.Column(db.DateTime, index = True, default = datetime.utcnow)
     user_id     = db.Column(db.Integer, db.ForeignKey('user.id'))
+    likes       = db.relationship('PostLike', backref='post', lazy='dynamic')
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
+class PostLike(db.Model):
+    __tablename__ = 'post_like'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
 
